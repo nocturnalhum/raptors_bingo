beb77e05beb1cfd7e34ea8cc70f260734f2d5a02
nocturnal_hum <thanin.chan@gmail.com>
2023-11-29 16:42:15 -0500

    Debug Analytics script.js 404 error

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,10 @@
+'use client';
+
 import './globals.css';
 import { Inter } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
-
-export const metadata = {
-  title: 'Raptors Game Day Bingo',
-  description: 'Raptors Bingo Game',
-};
 
 export default function RootLayout({ children }) {
   return (
