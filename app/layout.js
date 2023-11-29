a2430f39498d20d85c77eb5970d308031295b15d
nocturnal_hum <thanin.chan@gmail.com>
2023-11-29 16:58:10 -0500

    vercel deployDebug: Vercel Analytics script.js not loading in Production

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,9 @@ const inter = Inter({ subsets: ['latin'] });
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
+      <head>
+        <title>Raptors Bingo</title>
+      </head>
       <body className={inter.className}>
         {children}
         <Analytics />
